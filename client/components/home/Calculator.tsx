6343937b81866de84d77000bf532292efbb451df
Abhijith Babu <abijithb2002@gmail.com>
2025-01-02 20:43:27 +0530

    style: modified calculator styling

--- client/components/home/Calculator.tsx
+++ client/components/home/Calculator.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import CustomButton from '../ui/CustomButton';
-import { Icons } from '../common/Icons';
-import ProgressBar from './ProgressBar';
+import React, { useState, useEffect } from "react";
+import CustomButton from "../ui/CustomButton";
+import { Icons } from "../common/Icons";
+import ProgressBar from "./ProgressBar";
 
 const Calculator: React.FC = () => {
   const [amount, setAmount] = useState(10000000);
@@ -18,7 +18,7 @@ const Calculator: React.FC = () => {
       monthlyRate === 0
         ? amount / numberOfMonths
         : (amount * monthlyRate * Math.pow(1 + monthlyRate, numberOfMonths)) /
-        (Math.pow(1 + monthlyRate, numberOfMonths) - 1);
+          (Math.pow(1 + monthlyRate, numberOfMonths) - 1);
     const totalInterest = calculatedEmi * numberOfMonths - amount;
     setEmi(Math.round(calculatedEmi));
     setInterest(Math.round(totalInterest));
@@ -29,74 +29,117 @@ const Calculator: React.FC = () => {
     calculateValues();
   }, [amount, roi, tenure]);
 
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      const sliders =
+        document.querySelectorAll<HTMLInputElement>(".range-slider");
+
+      const updateThumbPosition = (slider: HTMLInputElement): void => {
+        const value = parseInt(slider.value, 10);
+        const max = parseInt(slider.max, 10);
+        const percentage = `${(value / max) * 100}%`;
+        slider.style.setProperty("--thumb-position", percentage);
+      };
+
+      sliders.forEach((slider) => {
+        slider.addEventListener("input", () => updateThumbPosition(slider));
+        updateThumbPosition(slider);
+      });
+    }
+  }, []);
+
   return (
-    <div id="emi-calculator" className="relative border-2 rounded-3xl w-full lg:w-2/3 p-4">
+    <div className="relative border-2 rounded-3xl w-full lg:w-2/3 p-4">
       <div className="flex flex-col md:flex-row gap-4 h-full">
         <div className="w-full md:w-3/5 flex flex-col justify-between">
           <div className="flex flex-col gap-4">
             <h1 className="font-black text-md">Calculate your Home Loan EMI</h1>
             <div className="flex flex-row gap-2 md:gap-4">
-              <div className='w-3/4 '>
-                <label className="block text-sm font-medium text-gray-900">Loan Amount</label>
+              <div className="w-3/4 ">
+                <label className="block text-sm font-medium text-gray-900">
+                  Loan Amount
+                </label>
                 <input
                   type="range"
+                  id="slider1"
                   min={100000}
                   max={100000000}
                   value={amount}
                   onChange={(e) => setAmount(Number(e.target.value))}
-                  className="w-full h-[2px] bg-gray-200 rounded-lg cursor-pointer"
+                  className="range-slider"
                 />
               </div>
-              <p className="w-1/4 flex items-center justify-center border-2 rounded-md text-sm">{amount}</p>
+              <p className="w-1/4 flex items-center justify-center border-2 rounded-md text-sm">
+                {amount.toLocaleString()}
+              </p>
             </div>
             <div className="flex flex-row gap-2 md:gap-4">
-              <div className='w-3/4 '>
-                <label className="block text-sm font-medium text-gray-900">Rate of Interest (p.a)</label>
+              <div className="w-3/4 ">
+                <label className="block text-sm font-medium text-gray-900">
+                  Rate of Interest (p.a)
+                </label>
                 <input
                   type="range"
+                  id="slider2"
                   min={1}
                   max={30}
                   value={roi}
                   onChange={(e) => setRoi(Number(e.target.value))}
-                  className="w-full h-[2px] bg-gray-200 rounded-lg cursor-pointer"
+                  className="range-slider"
                 />
               </div>
-              <p className="w-1/4 flex items-center justify-center border-2 rounded-md text-sm">{roi}%</p>
+              <p className="w-1/4 flex items-center justify-center border-2 rounded-md text-sm">
+                {roi}%
+              </p>
             </div>
             <div className="flex flex-row gap-2 md:gap-4">
-              <div className='w-3/4 '>
-                <label className="block text-sm font-medium text-gray-900">Loan Tenure</label>
+              <div className="w-3/4 ">
+                <label className="block text-sm font-medium text-gray-900">
+                  Loan Tenure
+                </label>
                 <input
                   type="range"
+                  id="slider3"
                   min={1}
                   max={30}
                   value={tenure}
                   onChange={(e) => setTenure(Number(e.target.value))}
-                  className="w-full h-[2px] bg-gray-200 rounded-lg cursor-pointer"
+                  className="range-slider"
                 />
               </div>
-              <p className="w-1/4 flex items-center justify-center border-2 rounded-md text-sm">{tenure} yr</p>
+              <p className="w-1/4 flex items-center justify-center border-2 rounded-md text-sm">
+                {tenure} yr
+              </p>
             </div>
           </div>
-          <div className="flex">
-            <p>Search Properties Based on Calculated Loan</p>
+          <div className="flex justify-between items-center py-6">
+            <p className="text-sm md:text-base">Search Properties Based on Calculated Loan</p>
             <CustomButton type="primary">
               Search <Icons.search />
             </CustomButton>
           </div>
         </div>
-        <div className="w-full md:w-2/5">
+        <div className="flex flex-col gap-2 w-full md:w-2/5 md:pt-8">
           <div className="flex flex-row gap-4">
-            <p className="w-2/3">Monthly EMI</p>
-            <p className="w-1/3">{emi}</p>
+            <p className="flex items-center w-2/3">
+              <span className="flex w-3 h-3 me-3 bg-purple-300 border-2 border-purple-500 rounded-full"></span>
+              Monthly EMI
+            </p>
+            <p className="w-1/3 text-right">{emi.toLocaleString()}</p>
           </div>
           <div className="flex flex-row gap-4">
-            <p className="w-2/3">Total Interest</p>
-            <p className="w-1/3">{interest}</p>
+            <p className="flex items-center w-2/3">
+              <span className="flex w-3 h-3 me-3 bg-gray-200 border-2 border-gray-400 rounded-full"></span>
+              Total Amount
+            </p>
+            <p className="w-1/3 text-right">{(interest + amount).toLocaleString()}</p>
           </div>
           <div className="flex flex-row gap-4">
-            <p className="w-2/3">Total Amount</p>
-            <p className="w-1/3">{interest + amount}</p>
+            <p className="flex items-center w-2/3">
+              <span className="flex w-3 h-3 me-3 bg-[#FFF4CA] border-2 border-[#C2AF64] rounded-full"></span>
+              Total Interest
+            </p>
+            <p className="w-1/3 text-right">{interest.toLocaleString()}</p>
           </div>
           <ProgressBar progress={percent} />
         </div>
--- client/components/home/ProgressBar.tsx
+++ client/components/home/ProgressBar.tsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import React from "react";
 
 const ProgressBar: React.FC<{ progress: number }> = ({ progress }) => {
   const radius = 110;
   const strokeWidth = 40;
   const circumference = 2 * Math.PI * radius;
   const strokeDashoffset = circumference - (progress / 100) * circumference;
+
   return (
     <div className="flex flex-col items-center justify-center relative">
-      <svg
-        className="transform -rotate-90 w-72 h-72"
-        width="290"
-        height="290"
-      >
+      <svg className="transform -rotate-90 w-72 h-72" width="290" height="290">
+        <defs>
+          <radialGradient
+            id="gradient1"
+            cx="50%"
+            cy="90%"
+            r="90%"
+            fx="1%"
+            fy="99%"
+          >
+            <stop offset="0%" stopColor="#FFF4CA" />
+            <stop offset="100%" stopColor="#C2AF64" />
+          </radialGradient>
+        </defs>
         <circle
           cx="145"
           cy="145"
@@ -25,16 +35,15 @@ const ProgressBar: React.FC<{ progress: number }> = ({ progress }) => {
           cx="145"
           cy="145"
           r={radius}
-          stroke="currentColor"
+          stroke="url(#gradient1)"
           strokeWidth={50}
           fill="transparent"
           strokeDasharray={circumference}
           strokeDashoffset={strokeDashoffset}
-          className="text-[#F2E6B8] outline-[#8E7D3A]"
         />
       </svg>
     </div>
-  )
-}
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
