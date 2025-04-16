d6699a8c4342409297535b772dd6e92602793ce1
Abhijith Babu <abijithb2002@gmail.com>
2025-04-16 13:14:28 +0530

    chore: minor update

--- client/components/ui/CustomImage.tsx
+++ client/components/ui/CustomImage.tsx
@@ -2,21 +2,8 @@ import React from "react";
 import type { ImageProps } from "next/image";
 
 const CustomImage = (props: ImageProps) => {
-  const { src, alt, width, height, style, ...rest } = props;
-  return (
-    <img
-      src={src as string}
-      alt={alt}
-      width={width}
-      height={height}
-      style={{
-        objectFit:
-          (props.objectFit as React.CSSProperties["objectFit"]) || "cover",
-        ...style,
-      }}
-      {...rest}
-    />
-  );
+  const { src, ...rest } = props;
+  return <img src={src as string} {...rest} />;
 };
 
 export default CustomImage;
